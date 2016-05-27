d782e3e55dd0b63d7390f5cec32344305485fe6b
Jersey Deng <jersdeng@gmail.com>
2016-05-26 23:39:43 -0700

    mini workflow to get timeseries of regions

--- first_level.py
+++ first_level.py
@@ -1,20 +1,24 @@
 # Import necessary modules
+from os.path import join as opj
+from nipype.interfaces.fsl import Merge, ImageMeants
 from nipype.interfaces.spm import Level1Design
 from nipype.interfaces.utility import Function, IdentityInterface
 from nipype.interfaces.io import SelectFiles, DataSink
 from nipype.pipeline.engine import Workflow, Node, MapNode
 
 # Specify variables
-subjdir = []
-seeds = []
+subjdir = ['/data/mridata/jdeng/tools/first_level/IC005-1/rsfmri']
+seeds = ['/data/mridata/jbrown/brains/rois/rPCC_sphere.nii',
+         '/data/mridata/jbrown/brains/rois/44rvFI_vAt12_C2vx123.nii',
+         '/data/mridata/jbrown/brains/rois/Boxer_DMidbrainTeg_sphere_3-5_-15_-8.nii']
+TR = 2.0
 
-#What type of Node to use for inputting subject paths and how to specify
-#that subjects have different paths (but all fitting a pattern:
+experiment_dir = '/data/mridata/jdeng/tools/first_level/nipype'
+output_dir = 'get_timeseries_output'
+working_dir = 'get_timeseries_workingdir'
 
-#What type of Node to use for looping over seeds? Iterable or MapNode
-#IdentityInterface.iterables(subjectID) > DataGrabber
-
-#Infosource
+## CREATE NODES
+# Infosource
 infosource = Node(IdentityInterface(fields=['subject_path', 'seed_path']), name="infosource")
 infosource.iterables = [('subject_path', subjdir), ('seed_path', seeds)]
 
@@ -22,3 +26,37 @@ infosource.iterables = [('subject_path', subjdir), ('seed_path', seeds)]
 templates = {'func': '{subject_path}/processedfmri_TRCNnSFmDI/swua_filteredf*.nii',
              'seed': '{seed_path}'}
 selectfiles = Node(SelectFiles(templates), name="selectfiles")
+
+# 1. Obtain timeseries for seed and nuisance variables
+# 1a. Merge all 3D functional images into a single 4D image
+# in_files <-- selectfiles func
+# out_files --> get_timeseries
+merge = Node(Merge(dimension = 't',
+                   output_type = 'NIFTI',
+                   tr = 2.0), name = 'merge')
+
+# 1b. Take mean of all voxels in mask at each timepoint
+# in_files <-- merge, selectfiles seed
+# out_files --> Datasink
+ts = Node(ImageMeants(), name = 'ts')
+
+## CREATE WORKFLOW
+# Create a short workflow to get the timeseries for seed + nuisance variables for each subject
+get_timeseries = Workflow(name='get_timeseries')
+get_timeseries.base_dir = opj(experiment_dir, working_dir)
+
+# Datasink
+datasink = Node(DataSink(base_directory=experiment_dir, container=output_dir), name="datasink")
+
+# Connect all components of the workflow
+get_timeseries.connect([
+    (infosource, selectfiles, [('subject_path', 'subject_path'), ('seed_path', 'seed_path')]),
+    (selectfiles, merge, [('func', 'in_files')]),
+    (merge, ts, [('merged_file', 'in_file')]),
+    (selectfiles, ts, [('seed', 'mask')]),
+    (ts, datasink, [('out_file', 'timeseries')])
+                ])
+
+# Visualize the workflow and run it
+get_timeseries.write_graph(graph2use='flat')
+get_timeseries.run()
