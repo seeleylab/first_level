5a63a1f0c553257a0940f56f6c527aa020c340cd
Jersey Deng <jersdeng@gmail.com>
2016-05-29 22:16:26 -0700

    structured pipeline to output seed+nuisance ts for each subject

--- first_level.py
+++ first_level.py
@@ -3,14 +3,15 @@ from os.path import join as opj
 from nipype.interfaces.fsl import Merge, ImageMeants
 from nipype.interfaces.spm import Level1Design
 from nipype.interfaces.utility import Function, IdentityInterface
+from nipype.interfaces.utility import Merge as utilMerge
 from nipype.interfaces.io import SelectFiles, DataSink
 from nipype.pipeline.engine import Workflow, Node, MapNode
 
 # Specify variables
 subjdir = ['IC005-1']
-seeds = ['/data/mridata/jbrown/brains/rois/rPCC_sphere.nii',
-         '/data/mridata/jbrown/brains/rois/44rvFI_vAt12_C2vx123.nii',
-         '/data/mridata/jbrown/brains/rois/Boxer_DMidbrainTeg_sphere_3-5_-15_-8.nii']
+seeds = ['rPCC_sphere.nii',
+         '44rvFI_vAt12_C2vx123.nii',
+         'Boxer_DMidbrainTeg_sphere_3-5_-15_-8.nii']
 nuisance_masks = ['/data/mridata/SeeleyToolbox/SeeleyFirstLevel/proc/csf_ant_post_bilateral.nii',
                   '/data/mridata/SeeleyToolbox/SeeleyFirstLevel/proc/avg152T1_white_mask.nii']
 TR = 2.0
@@ -20,18 +21,21 @@ output_dir = 'get_timeseries_output'
 
 ## CREATE NODES
 # For distributing subject paths
-infosource = Node(IdentityInterface(fields=['subject_name']),
+infosource = Node(IdentityInterface(fields=['subject_name', 'seed_name']),
                   name="infosource")
-infosource.iterables = [('subject_name', subjdir)]
+infosource.iterables = [('subject_name', subjdir), ('seed_name', seeds)]
 
-templates = {'func': '/data/mridata/jdeng/tools/first_level/{subject_name}/rsfmri/processedfmri_TRCNnSFmDI/images/swua_filteredf*.nii'}
+templates = {'func': '/data/mridata/jdeng/tools/first_level/{subject_name}/rsfmri/processedfmri_TRCNnSFmDI/images/swua_filteredf*.nii',
+             'seed': '/data/mridata/jbrown/brains/rois/{seed_name}'}
 selectfiles = Node(SelectFiles(templates), name="selectfiles")
 
 # For distributing seed and nuisance mask paths
-rois = seeds + nuisance_masks
+# Merge Node to combine seed mask and nuisance mask paths
+seed_plus_nuisance = Node(utilMerge(2), name = 'seed_plus_nuisance')
+seed_plus_nuisance.inputs.in2 = nuisance_masks
 
+# For distributing seed and nuisance mask paths
 distributor = Node(IdentityInterface(fields=['rois']), name='distributor')
-distributor.inputs.rois = rois
 
 # 1. Obtain timeseries for seed and nuisance variables
 # 1a. Merge all 3D functional images into a single 4D image
@@ -50,13 +54,18 @@ get_timeseries.base_dir = experiment_dir
 # Datasink
 datasink = Node(DataSink(base_directory=experiment_dir, container=output_dir), name="datasink")
 
+substitutions = [('_subject_name_', '_'), ('_seed_name_', '')]
+datasink.inputs.substitutions = substitutions
+
 # Connect all components of the workflow
 get_timeseries.connect([
-    (infosource, selectfiles, [('subject_name', 'subject_name')]),
+    (infosource, selectfiles, [('subject_name', 'subject_name'), ('seed_name', 'seed_name')]),
     (selectfiles, merge, [('func', 'in_files')]),
     (merge, ts, [('merged_file', 'in_file')]),
+    (selectfiles, seed_plus_nuisance, [('seed', 'in1')]),
+    (seed_plus_nuisance, distributor, [('out', 'rois')]),
     (distributor, ts, [('rois', 'mask')]),
-    (ts, datasink, [('out_file', 'seed_timeseries')])
+    (ts, datasink, [('out_file', 'timeseries')])
                 ])
 
 # Visualize the workflow and run it
